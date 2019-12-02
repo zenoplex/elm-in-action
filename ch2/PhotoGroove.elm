687b8cb255b563da43572ec164c4de7b42bf44c9
Gen Ichino <zenoplex@gmail.com>
2019-12-03 01:33:20 +0900

    feat: added size chooser

--- ch2/PhotoGroove.elm
+++ ch2/PhotoGroove.elm
@@ -2,15 +2,21 @@ module PhotoGroove exposing (main)
 import Browser
 import Array exposing (Array)
 import Html exposing (Html)
-import Html.Attributes exposing (id, class, classList, src)
+import Html.Attributes exposing (id, class, classList, src, type_, name)
 import Html.Events exposing (onClick)
 
 type alias Photo =
     { url : String }
 
+type ThumbnailSize
+  = Small
+  | Medium 
+  | Large
+
 type alias Model =
     { photos : List Photo
     , selectedUrl : String
+    , chosenSize: ThumbnailSize
     }
 
 type alias Msg =
@@ -26,6 +32,7 @@ initialModel =
   ,{ url = "3.jpeg" }
   ]
   , selectedUrl = "1.jpeg"
+  , chosenSize = Medium
   }
 
 photoArray: Array Photo
@@ -35,10 +42,35 @@ urlPrefix: String
 urlPrefix = 
   "http://elm-in-action.com/"
 
+sizeToString: ThumbnailSize -> String
+sizeToString size =
+  case size of
+    Small ->
+      "small"
+    Medium ->
+      "medium"
+    Large ->
+      "large" 
+
+viewSizeChooser: ThumbnailSize -> Html Msg
+viewSizeChooser size =
+  Html.label []
+    [
+      Html.input [ type_ "radio", name "size"] []
+    , Html.text (sizeToString size)
+    ]
+
 view: Model -> Html Msg
 view model = 
   Html.div [ class "content" ]
     [ Html.h1 [] [ Html.text "Photo Groove" ]
+    , Html.h3 [] [ Html.text "Thumbnail size"]
+    , Html.div [ id "choose-size" ]
+      [
+        viewSizeChooser Small
+      , viewSizeChooser Medium
+      , viewSizeChooser Large
+     ]
     , Html.button [ onClick { description = "ClickedSurprizeMe", data = ""} ] [ Html.text "Surprize Me!"]
     , Html.div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
     , Html.img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl)][]
