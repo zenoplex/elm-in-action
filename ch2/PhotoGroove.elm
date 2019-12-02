47723e32e4f0b83cab813c0b41b4e400be6fce2c
Gen Ichino <zenoplex@gmail.com>
2019-12-03 01:17:57 +0900

    feat: added type annotations

--- ch2/PhotoGroove.elm
+++ ch2/PhotoGroove.elm
@@ -1,9 +1,24 @@
 module PhotoGroove exposing (main)
 import Browser
-import Html
+import Array exposing (Array)
+import Html exposing (Html)
 import Html.Attributes exposing (id, class, classList, src)
 import Html.Events exposing (onClick)
 
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+type alias Msg =
+    { description : String
+    , data : String
+    }
+
+initialModel: Model
 initialModel = 
   { photos = 
   [{ url = "1.jpeg" }
@@ -13,16 +28,23 @@ initialModel =
   , selectedUrl = "1.jpeg"
   }
 
+photoArray: Array Photo
+photoArray = Array.fromList initialModel.photos
+
+urlPrefix: String
 urlPrefix = 
   "http://elm-in-action.com/"
 
+view: Model -> Html Msg
 view model = 
   Html.div [ class "content" ]
     [ Html.h1 [] [ Html.text "Photo Groove" ]
+    , Html.button [ onClick { description = "ClickedSurprizeMe", data = ""} ] [ Html.text "Surprize Me!"]
     , Html.div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
     , Html.img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl)][]
     ]
 
+viewThumbnail: String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
   Html.img
     [ 
@@ -31,12 +53,15 @@ viewThumbnail selectedUrl thumb =
     , onClick { description = "ClickedPhoto", data = thumb.url}
     ][]
 
+update: Msg -> Model -> Model
 update msg model =
-  if msg.description == "ClickedPhoto" then
-    { model | selectedUrl = msg.data }
-  else
-    model
-
+ case msg.description of
+    "ClickedPhoto" ->
+      { model | selectedUrl = msg.data }
+    "ClickedSurprizeMe" ->
+      { model | selectedUrl = "2.jpeg" }
+    _ ->
+      model
 main =
   Browser.sandbox
     { 
