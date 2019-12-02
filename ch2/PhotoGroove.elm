5b142e33c82cf8cf175edc36f188515ed0770eaa
Gen Ichino <zenoplex@gmail.com>
2019-12-02 22:51:13 +0900

    feat: ch2 basic model

---
+++ ch2/PhotoGroove.elm
@@ -0,0 +1,25 @@
+module PhotoGroove exposing (main)
+import Html
+import Html.Attributes exposing (..)
+
+initialModel = 
+  [{ url = "1.jpeg" }
+  ,{ url = "2.jpeg" }
+  ,{ url = "3.jpeg" }
+  ]
+
+urlPrefix = 
+  "http://elm-in-action.com/"
+
+view model = 
+  Html.div [ class "content" ]
+    [ Html.h1 [] [ Html.text "Photo Groove" ]
+    , Html.div [ id "thumbnails" ] (List.map viewThumbnail model)
+    ]
+
+viewThumbnail thumb =
+  Html.img [ src(urlPrefix ++ thumb.url)][]
+              
+              
+main =
+  view initialModel
